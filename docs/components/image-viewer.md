84a98f538a9a5e955a94be1be1d5a21298476d88
Eric Melin <eric.melin@sitevision.se>
2017-06-09 10:16:35 +0200

    SV-22969, image-viewer Re-Do

--- docs/components/image-viewer.md
+++ docs/components/image-viewer.md
@@ -6,33 +6,22 @@ group: components
 ---
 {% example html %}
 <div data-image-viewer class="{{ site.css_prefix }}-image-viewer">
-   <div class="{{ site.css_prefix }}-image-viewer {{ site.css_prefix }}-image-viewer--slide {{ site.css_prefix }}-image-viewer__inner">
-      <div class="{{ site.css_prefix }}-image-viewer__item {{ site.css_prefix }}-image-viewer__item--active">
-         <img tabindex="-1" src="https://unsplash.it/800/600/?blur" class="{{ site.css_prefix }}-embedded__img" alt="First slide">
-      </div>
-      <div class="{{ site.css_prefix }}-image-viewer__item">
-         <img tabindex="-1" src="https://unsplash.it/800/600/" class="{{ site.css_prefix }}-embedded__img" alt="Second slide">
-      </div>
-      <div class="{{ site.css_prefix }}-image-viewer__item">
-         <img tabindex="-1" src="https://unsplash.it/800/600/?blur" class="{{ site.css_prefix }}-embedded__img" alt="Third slide">
-      </div>
+   <div>   
+      <a class="{{ site.css_prefix }}-image-viewer-images" href="//unsplash.it/900/400/?blur" data-image="0">
+         <img src="https://unsplash.it/900/400/?blur" class="{{ site.css_prefix }}-embedded__img" alt="Image">
+      </a>
    </div>
    <ol class="{{ site.css_prefix }}-image-viewer__thumbnails">
-      <li data-move-to="0">
-         <a href="//unsplash.it/800/600/?blur">
-            <img src="https://unsplash.it/800/600/?blur" class="{{ site.css_prefix }}-image-viewer__thumbnail {{ site.css_prefix }}-image-viewer__item--active" alt="First thumbnail">
+      <li>
+         <a class="{{ site.css_prefix }}-image-viewer-images" href="//unsplash.it/600/400" data-image="1">
+            <img src="https://unsplash.it/200/100" class="{{ site.css_prefix }}-image-viewer__thumbnail"
+               alt="First thumbnail">
          </a>
       </li>
-      <li data-move-to="1">
-         <a href="//unsplash.it/800/600/">
-            <img src="https://unsplash.it/800/600/" class="{{ site.css_prefix }}-image-viewer__thumbnail"
+       <li>
+         <a class="{{ site.css_prefix }}-image-viewer-images" href="//unsplash.it/800/400" data-image="2">
+            <img src="https://unsplash.it/200/100" class="{{ site.css_prefix }}-image-viewer__thumbnail"
                alt="Second thumbnail">
-         </a>
-      </li>
-      <li data-move-to="2">
-         <a href="//unsplash.it/800/600/?blur">
-            <img src="https://unsplash.it/800/600/?blur" class="{{ site.css_prefix }}-image-viewer__thumbnail"
-               alt="Third thumbnail">
          </a>
       </li>
    </ol>
--- src/js/image-viewer.js
+++ src/js/image-viewer.js
@@ -19,52 +19,24 @@ const Imageviewer = (($) => {
    const ESCAPE_KEY = 27;
    const ARROW_LEFT_KEYCODE = 37;
    const ARROW_RIGHT_KEYCODE = 39;
-   const TOUCHEVENT_WAIT = 500;
-   const SLIDE_WIDTH_PX = 80;
-
-   const DEFAULTS = {
-      keyboard: true,
-      slide: false,
-      swipe: true,
-      wrap: true
-   };
 
    const SELECTORS = {
-      ACTIVE               : '.sv-image-viewer__item--active',
       ACTIVE_DOT           : '.sv-image-viewer__indicators--active',
-      ACTIVE_ITEM          : '.sv-image-viewer__item--active.sv-image-viewer__item',
-      DATA_SLIDE_TO        : '[data-slide-to]',
       DATA_MOVE_TO         : '[data-move-to]',
       DATA_MOVE            : '[data-move]',
-      DATA_IMAGE_VIEWER    : '[data-image-viewer], [data-move-to]',
-      CLOSE_BTN            : '.sv-image-viewer__close-btn',
+      DATA_IMAGE_VIEWER    : '[data-image-viewer]',
+      IMAGES               : '.sv-image-viewer-images',
       INDICATORS           : '.sv-image-viewer__indicators',
-      INNER                : '.sv-image-viewer__inner',
-      ITEM                 : '.sv-image-viewer__item',
-      NEXT_PREV            : '.sv-image-viewer--next, .sv-image-viewer--prev'
+      DATA_SLIDE_TO        : '[data-slide-to]'
    };
 
    const ClassName = {
-      ACTIVE         : 'sv-image-viewer__item--active',
       ACTIVE_DOT     : 'sv-image-viewer__indicators--active',
-      LEFT           : 'sv-image-viewer__item--left',
-      NEXT           : 'sv-image-viewer__item--next',
-      PREV           : 'sv-image-viewer__item--prev',
-      RIGHT          : 'sv-image-viewer__item--right',
-      SLIDE          : 'sv-image-viewer--slide'
-   };
-
-   const Direction = {
-      LEFT  : 'left',
-      NEXT  : 'next',
-      PREV  : 'prev',
-      RIGHT : 'right'
+      HIDDEN         : 'sv-image-viewer--hidden'
    };
 
    const Events = {
       CLICK_DATA_API : `click${EVENT_KEY}${DATA_API_KEY}`,
-      SLID           : `slid${EVENT_KEY}`,
-      SLIDE          : `slide${EVENT_KEY}`,
       TOUCHEND       : `touchend${EVENT_KEY}`,
       TOUCHMOVE      : `touchmove${EVENT_KEY}`,
       TOUCHSTART     : `touchstart${EVENT_KEY}`
@@ -74,150 +46,106 @@ const Imageviewer = (($) => {
 
       constructor(element, config) {
          this.$el = $(element);
-         this.$images = this.$el.find(SELECTORS.ITEM);
-
-         this.$popup = this.$el.clone();
-         this.$popupImages = this.$popup.children();
-         this._isSliding = false;
+         this.$images = this.$el.find(SELECTORS.IMAGES);
 
          this._isShown = false;
 
-         this.config = $.extend({}, DEFAULTS, this.$popup.data(), config);
+         this.config = $.extend({}, this.$el.data(), config);
       }
 
       next() {
-         if (!this._isSliding) {
-            this._slide(Direction.NEXT);
-         }
+         this.show(this._getNextItemIndex());
       }
 
       prev() {
-         if (!this._isSliding) {
-            this._slide(Direction.PREV);
-         }
+         this.show(this._getPrevItemIndex());
       }
 
-      show() {
-         if (this._isShown) {
-            return;
+      show(index) {
+         this.config.index = index;
+
+         if (!this._isShown) {
+
+            this.$modal = $('<div/>', {
+               class: 'sv-image-viewer__modal'
+            });
+
+            this.$btnContainer = $('<div/>', {
+               class: 'sv-image-viewer__dialog'
+            });
+
+            this.$imgContainer = $('<div/>', {
+               class: 'sv-image-viewer__modal-container'
+            });
+
+            this.$modal.append(this.$btnContainer);
+            this.$btnContainer.append(this.$imgContainer);
+            this.$modal.appendTo(document.body);
+
+            const $image = $(this.$images[index])[0];
+            const href = $image.getAttribute('href');
+
+            this.$btnContainer.append(this._getIndicators());
+
+            if (this.$images.length > 1) {
+               this.$btnContainer.append(this._getButtons());
+            }
+
+            this.$btnContainer.addClass(ClassName.HIDDEN);
+            const $downloadingImage = this._loadImage(href);
+
+            this.$imgContainer.html($downloadingImage);
+
+            this._isShown = true;
+
+            this._bindContainerEvents();
+            this._bindEvents();
+            this._showBackdrop();
+
+         } else {
+            this.$btnContainer.addClass(ClassName.HIDDEN);
+            const $image = $(this.$images[index])[0];
+            const href = $image.getAttribute('href');
+            const $downloadingImage = this._loadImage(href);
+
+            this.$imgContainer.html($downloadingImage);
+            this._setActiveIndicatorElement();
+
+            this._isShown = true;
          }
-
-         Util.reflow(this.$popup);
-
-         this.$modal = $('<div/>', {
-            class: 'sv-image-viewer__modal'
-         });
-
-         this.$container = $('<div/>', {
-            class: 'sv-image-viewer__modal-container'
-         });
-
-         this.$modal.append(this.$container);
-         this.$modal.appendTo(document.body);
-
-         if (this.$popupImages.length > 1) {
-            this.$popup.append(this._getButtons());
-         }
-
-         this.$container.append(this._getIndicators());
-
-         this._bindContainerEvents();
-
-         this.$popup.append(this._getCloseButton());
-         this.$popup.appendTo(this.$container)
-            .addClass('sv-image-viewer__inner__dialog');
-         this._showBackdrop();
-
-         if (this.config.moveTo) {
-            this.$popup.removeClass(ClassName.SLIDE);
-            this.goTo(this.config.moveTo);
-            this.$popup.addClass(ClassName.SLIDE);
-         }
-
-         this._isShown = true;
-
-         this._bindEvents();
-
-         this.$popup.find(SELECTORS.ACTIVE).children().focus();
       }
 
       hide() {
          const removeBackdropCallback = () => {
             this.$backdrop.remove();
             this.$modal.remove();
-            this.$popup.remove();
-            this.$container.remove();
+            this.$btnContainer.remove();
+            this.$imgContainer.remove();
          };
 
          this.$modal.removeClass('sv-image-viewer__modal');
 
-         this._unbindEvents();
-
          this.$backdrop
             .one(Util.getTransitionEndEvent(), removeBackdropCallback)
             .removeClass(BACKDROP_ANIMATION);
+
          this._isShown = false;
       }
 
-      goTo(index) {
-         this._activeElement = this.$popup.find(SELECTORS.ACTIVE_ITEM)[0];
+      _loadImage(href) {
+         const $downloadingImage = $('<img>');
+         $downloadingImage.addClass('sv-embedded__img');
+         $downloadingImage.attr('tabindex', '-1');
+         $downloadingImage.on('load', () => {
+            this.$btnContainer.removeClass(ClassName.HIDDEN);
+            this.$imgContainer.children()[0].focus();
+         });
 
-         const activeIndex = this._getItemIndex(this._activeElement);
-
-         if (index > this.$images.length - 1 || index < 0) {
-            return;
-         }
-
-         if (this._isSliding) {
-            return;
-         }
-
-         const direction = index > activeIndex ?
-            Direction.NEXT :
-            Direction.PREV;
-
-         this._slide(direction, this.$popupImages[index]);
-      }
-
-      // Mobile slide
-      startTouchSlide(event) {
-         this.touchstartx =  event.originalEvent.touches[0].pageX;
-      }
-
-      moveTouchSlide(event) {
-         this.touchmovex =  event.originalEvent.touches[0].pageX;
-
-         this.movex = -(this.touchstartx - this.touchmovex);
-
-         this.moved = false;
-         const activeItem = this.$popup.find(SELECTORS.ACTIVE).children();
-         activeItem.css('transform', `translate3d(${this.movex}px, 0, 0)`);
-
-         if (this.movex > SLIDE_WIDTH_PX) {
-            this.prev();
-            this.moved = true;
-            setTimeout(() => {
-               activeItem.css('transform', 'translate3d(0, 0, 0)');
-            }, TOUCHEVENT_WAIT);
-         } else if (this.movex < -SLIDE_WIDTH_PX) {
-            this.next();
-            this.moved = true;
-            setTimeout(() => {
-               activeItem.css('transform', 'translate3d(0, 0, 0)');
-            }, TOUCHEVENT_WAIT);
-         }
-      }
-
-      endTouchSlide() {
-         if (!this.moved) {
-            this.$popup.find(SELECTORS.ACTIVE)
-               .children()
-               .css('transform', 'translate3d(0, 0, 0)');
-         }
+         return $downloadingImage.attr('src', href);
       }
 
       _bindContainerEvents() {
-         this.$container.on(Events.CLICK_DATA_API, SELECTORS.DATA_MOVE, (e) => {
+         this.$btnContainer.on(Events.CLICK_DATA_API, SELECTORS.DATA_MOVE, (e) => {
             e.preventDefault();
 
             if ($(e.currentTarget).data('move') === 'next') {
@@ -227,11 +155,11 @@ const Imageviewer = (($) => {
             }
          });
 
-         this.$container.on(Events.CLICK_DATA_API, SELECTORS.DATA_SLIDE_TO, (e) => {
+         this.$btnContainer.on(Events.CLICK_DATA_API, SELECTORS.DATA_SLIDE_TO, (e) => {
             const viewerIndex = e.currentTarget.getAttribute('data-slide-to');
 
             if (viewerIndex) {
-               this.goTo(viewerIndex);
+               this.show(viewerIndex);
             }
          });
       }
@@ -260,10 +188,9 @@ const Imageviewer = (($) => {
 
       _getIndicatorItems() {
          let html = '';
-         const activeElement = this.$popup.find(SELECTORS.ACTIVE_ITEM)[0];
-         const activeElementIndex = this._getItemIndex(activeElement);
+         const activeElementIndex = this.config.index;
 
-         for (let i = 0; i < this.$popupImages.length; i++) {
+         for (let i = 0; i < this.$images.length; i++) {
             if (activeElementIndex !== i) {
                html += `<li data-slide-to="${i}">
                   <span class="sv-icon--dot-small sv-icon--large"></span>
@@ -276,10 +203,6 @@ const Imageviewer = (($) => {
          }
 
          return html;
-      }
-
-      _getCloseButton() {
-         return '<span class="sv-image-viewer__close-btn sv-icon--deleted sv-icon--large"></span>';
       }
 
       _showBackdrop() {
@@ -315,58 +238,9 @@ const Imageviewer = (($) => {
          $(e.currentTarget).removeClass(ANIMATION);
       }
 
-      _getItemByDirection(direction, activeElement) {
-         const isNextDirection = direction === Direction.NEXT;
-         const activeIndex = this._getItemIndex(activeElement);
-         const lastImageIndex = this.$popupImages.length - 1;
-         const isGoingToWrap = (!isNextDirection && activeIndex === 0) || (isNextDirection && activeIndex === lastImageIndex);
-
-         if (isGoingToWrap && !this.config.wrap) {
-            return activeElement;
-         }
-
-         const delta = isNextDirection ? 1 : -1;
-         const itemIndex = (activeIndex + delta) % this.$popupImages.length;
-
-         return itemIndex === -1 ?
-            this.$popupImages[lastImageIndex] : this.$popupImages[itemIndex];
-      }
-
       _bindEvents() {
-         if (this.config.keyboard) {
-            this.$popup
-               .on('keydown', (event) => this._keydown(event));
-         }
-         if (this.config.swipe && Util.isTouch()) {
-            this._bindTouchSlider();
-         }
-
-         this.$popup
-            .on(Events.CLICK_DATA_API, SELECTORS.CLOSE_BTN, () => {
-               this.hide();
-            });
-      }
-
-      _bindTouchSlider() {
-         const container = this.$popup;
-         container.on(Events.TOUCHSTART,  (event) => {
-            this.startTouchSlide(event);
-         });
-
-         container.on(Events.TOUCHMOVE, (event) => {
-            this.moveTouchSlide(event);
-         });
-
-         container.on(Events.TOUCHEND, () => {
-            this.endTouchSlide(event);
-         });
-      }
-
-
-      _unbindEvents() {
-         this.$popup.off('click keydown');
-         this.$modal.off('click');
-         this.$container.off('click keydown');
+         this.$imgContainer
+            .on('keydown', (event) => this._keydown(event));
       }
 
       _keydown(event) {
@@ -394,121 +268,26 @@ const Imageviewer = (($) => {
          }
       }
 
-      _getItemIndex(element) {
-         return this.$popupImages.index(element);
+      _getNextItemIndex() {
+         return Number(this.config.index) === this.$images.length - 1 ? 0 : parseInt(this.config.index, 10) + 1;
+      }
+      _getPrevItemIndex() {
+         return Number(this.config.index) === 0 ? this.$images.length - 1 : parseInt(this.config.index, 10) - 1;
       }
 
-      _triggerSlideEvent(targetElement, eventDirectionName) {
-         const targetIndex = this._getItemIndex(targetElement);
-         const fromIndex = this._getItemIndex(this.$popup.find(SELECTORS.ACTIVE_ITEM)[0]);
-         const slideEvent = $.Event(Events.SLIDE, {
-            targetElement,
-            direction: eventDirectionName,
-            from: fromIndex,
-            to: targetIndex
-         });
-
-         this.$popup.trigger(slideEvent);
-
-         return slideEvent;
-      }
-
-      _setActiveIndicatorElement(element) {
-         this._indicatorsElement = this.$container.find(SELECTORS.INDICATORS)[0];
-         if (this._indicatorsElement) {
-            $(this._indicatorsElement)
+      _setActiveIndicatorElement() {
+         this._indicatorsElement = this.$btnContainer.find(SELECTORS.INDICATORS)[0];
+         if (this.$images.length > 0) {
+            $(this.$btnContainer)
                .find(SELECTORS.ACTIVE_DOT)
                .removeClass(ClassName.ACTIVE_DOT);
 
-            const nextIndicator = this._indicatorsElement.children[this._getItemIndex(element)].firstElementChild;
+            const indicators = this.$btnContainer.find(SELECTORS.INDICATORS).children()[this.config.index];
 
-            if (nextIndicator) {
-               $(nextIndicator).addClass(ClassName.ACTIVE_DOT);
+            if (indicators) {
+               $(indicators).children()
+                  .addClass(ClassName.ACTIVE_DOT);
             }
-         }
-      }
-
-      _slide(direction, element) {
-         const activeElement = this.$popup.find(SELECTORS.ACTIVE_ITEM)[0];
-         const activeElementIndex = this._getItemIndex(activeElement);
-         const nextElement = element || activeElement && this._getItemByDirection(direction, activeElement);
-         const nextElementIndex = this._getItemIndex(nextElement);
-
-         let directionalClassName;
-         let orderClassName;
-         let eventDirectionName;
-
-         if (direction === Direction.NEXT) {
-            directionalClassName = ClassName.LEFT;
-            orderClassName = ClassName.NEXT;
-            eventDirectionName = Direction.LEFT;
-         } else {
-            directionalClassName = ClassName.RIGHT;
-            orderClassName = ClassName.PREV;
-            eventDirectionName = Direction.RIGHT;
-         }
-
-         const $nextElement = $(nextElement);
-
-         if (nextElement && $nextElement.hasClass(ClassName.ACTIVE)) {
-            this._isSliding = false;
-            return;
-         }
-
-         const slideEvent = this._triggerSlideEvent(nextElement, eventDirectionName);
-
-         if (slideEvent.isDefaultPrevented()) {
-            return;
-         }
-
-         if (!activeElement || !nextElement) {
-            return;
-         }
-
-         this._isSliding = true;
-
-         this._setActiveIndicatorElement(nextElement);
-
-         const slidEvent = $.Event(Events.SLID, {
-            relatedTarget: nextElement,
-            direction: eventDirectionName,
-            from: activeElementIndex,
-            to: nextElementIndex
-         });
-
-         const $activeElement = $(activeElement);
-
-         if (this.$popup.hasClass(ClassName.SLIDE)) {
-
-            $nextElement.addClass(orderClassName);
-
-            Util.reflow(nextElement);
-
-            $activeElement.addClass(directionalClassName);
-            $nextElement.addClass(directionalClassName);
-
-            $activeElement
-               .one(Util.getTransitionEndEvent(), () => {
-
-                  $nextElement
-                     .removeClass(`${directionalClassName} ${orderClassName}`)
-                     .addClass(ClassName.ACTIVE);
-
-                  $activeElement
-                     .removeClass(`${ClassName.ACTIVE} ${orderClassName} ${directionalClassName}`);
-
-                  this._isSliding = false;
-
-                  this.$popup.trigger(slidEvent);
-                  this.$popup.find(SELECTORS.ACTIVE).children().focus();
-               });
-         } else {
-            $activeElement.removeClass(ClassName.ACTIVE);
-            $nextElement.addClass(ClassName.ACTIVE);
-
-            this._isSliding = false;
-            this.$popup.trigger(slideEvent);
-            this.$popup.find(SELECTORS.ACTIVE).children().focus();
          }
       }
 
@@ -516,22 +295,14 @@ const Imageviewer = (($) => {
          return this.each(() => {
             const $this = $(this);
 
-            const _config = $.extend({}, DEFAULTS, $this.data());
+            const _config = $this.data();
 
             if (typeof config === 'object') {
                $.extend(_config, config);
             }
 
-            const action = typeof config === 'string' ? config : 'show';
-
             const data = new Imageviewer(this, _config);
-
-            if (typeof action === 'string') {
-               if (data[action] === undefined) {
-                  throw new Error(`No method named "${action}"`);
-               }
-               data[action]();
-            }
+            data.show(_config.index);
          });
       }
    }
@@ -540,18 +311,13 @@ const Imageviewer = (($) => {
       .on(Events.CLICK_DATA_API, SELECTORS.DATA_IMAGE_VIEWER, function(e) {
          e.preventDefault();
 
-         let $target;
-         if (this.hasAttribute('data-image-viewer') && !$(e.target).hasClass('sv-image-viewer__thumbnail')) {
-            $target = $(e.currentTarget).find(SELECTORS.INNER);
-         } else if (this.getAttribute('data-move-to')) {
-            $target = $(e.currentTarget).parent().siblings(SELECTORS.INNER);
-         }
+         const $target = $(this);
 
          if (!$target) {
             return;
          }
-
          const config = $.extend({}, $target.data(), $(this).data());
+         config.index = Number(e.target.parentElement.getAttribute('data-image'));
 
          $target.imageviewer(config);
       });
