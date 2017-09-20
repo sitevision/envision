cf4558d5a9000b265b57336c34c15435410a5ebc
Albin Öhrn <albinohrn@gmail.com>
2017-09-20 16:37:45 +0200

    Added svg shim for IE11

---
+++ src/js/shims.js
@@ -0,0 +1,10 @@
+/**
+ * --------------------------------------------------------------------------
+ * Licensed under MIT https://github.com/sitevision/envision/blob/master/LICENSE
+ * --------------------------------------------------------------------------
+ */
+
+ /**
+  * Make use of external svgs possible in IE 11
+  */
+import 'svgxuse';
