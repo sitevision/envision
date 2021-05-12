d983a1286fa4761c43925972b0419f28edb88b35
Albin Öhrn <albin.ohrn@sitevision.se>
2021-05-12 10:42:22 +0200

    Updated 404 with link home. Closes #232

--- packages/envision-docs/src/pages/404.js
+++ packages/envision-docs/src/pages/404.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const FourOFour = () => {
-   return <div>Hello, World!</div>;
+   return (
+      <div className="env-p-around--large">
+         <h1 className="env-text">404 - Page not found</h1>
+         <p className="env-text">
+            Oops! That page does not exist. <a href="/">Home</a>
+         </p>
+      </div>
+   );
 };
 
 export default FourOFour;
