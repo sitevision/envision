b42042c629fba8d401666ea1fc960d3844aeea24
Henrik Ekelöf <henrik.ekelof@sitevision.se>
2021-09-17 16:52:01 +0200

    Moved modal and dropdown to body

--- packages/envision/src/js/dropdown.js
+++ packages/envision/src/js/dropdown.js
@@ -3,38 +3,103 @@
  * Licensed under MIT https://github.com/sitevision/envision/blob/master/LICENSE
  * --------------------------------------------------------------------------
  */
+import { getNodes, hide, unhide } from './util/nodes';
 
-import $ from 'jquery';
-
-const NAME = 'envDropdown';
 const ENV_DROPDOWN_OPEN = 'env-is-open';
 const TOGGLE_DROPDOWN = '[data-dropdown]';
-const ENV_DROPDOWN = '.env-dropdown';
-const ENV_CLICK_EVENT = 'click.env-dropdown';
-const ENV_KEYDOWN_EVENT = 'keydown.env-dropdown';
-const ENV_KEYUP_EVENT = 'keyup.env-dropdown';
+const TARGET_ATTR = 'data-target';
+const INDEX_ATTR = 'data-env-dropdown-index';
 const ENV_DROPDOWN_MENU = '.env-dropdown__menu';
-const RIGHT_MOUSE_BUTTON = 2;
-const KEYS = {
-   ESCAPE_KEY: 'Escape',
-   ARROWDOWN: 'ArrowDown',
-   ARROWUP: 'ArrowUp',
-   SPACE: 32,
-   TAB_KEY: 'Tab',
+const DATA_INITIALIZED = 'data-env-dropdown';
+
+const dropdowns = {};
+
+let Popper;
+const getPopper = async () => {
+   if (Popper) {
+      return Popper;
+   }
+
+   const { default: DynamicPopper } = await import(
+      /* webpackChunkName: "popper" */ 'popper.js'
+   );
+   Popper = DynamicPopper;
+   return Popper;
 };
-const REGEXP_KEYDOWN = new RegExp(
-   `${KEYS.ARROWUP}|${KEYS.ARROWDOWN}|${KEYS.ESCAPE_KEY}`
-);
 
 class Dropdown {
-   constructor(element) {
-      this.el = element;
-      this.$el = $(element);
-      this._id = this.el.id;
+   constructor(container, menu, button) {
+      this.button = button;
+      this.container = container;
+      this.placeholder = document.createElement('span');
+      this.menu = menu;
+      this.menuitems = getNodes('.env-dropdown__item', this.menu);
+      this.menu.insertAdjacentElement('beforebegin', this.placeholder);
+
+      this.menuitems.forEach((el, i) => {
+         el.setAttribute(INDEX_ATTR, i);
+      });
+
+      this._handleClick = (() => {
+         this.hide();
+      }).bind(this);
+
+      this._handleKeyDown = ((e) => {
+         const el = e.target;
+         const menuItemCount = this.menuitems.length;
+
+         if (e.key === 'Escape') {
+            this.hide();
+         } else if (this.button.contains(el)) {
+            if (
+               ((e.key === 'Tab' && !e.shiftKey) || e.key === 'ArrowDown') &&
+               menuItemCount > 0
+            ) {
+               this.menuitems[0].focus();
+               e.preventDefault();
+            }
+         } else if (this.menu.contains(el) && menuItemCount > 0) {
+            const current = parseInt(el.getAttribute(INDEX_ATTR), 10);
+            if (
+               e.key === 'Tab' &&
+               !e.shiftKey &&
+               current === menuItemCount - 1
+            ) {
+               this.button.focus();
+               this.hide();
+            } else if (e.key === 'Tab' && e.shiftKey && current === 0) {
+               this.button.focus();
+               e.preventDefault();
+            } else if (e.key === 'ArrowUp') {
+               e.preventDefault();
+               const prev = this.menu.querySelector(
+                  '[' + INDEX_ATTR + '="' + (current - 1) + '"]'
+               );
+               if (prev) {
+                  prev.focus();
+               }
+            } else if (e.key === 'ArrowDown') {
+               e.preventDefault();
+               const next = this.menu.querySelector(
+                  '[' + INDEX_ATTR + '="' + (current + 1) + '"]'
+               );
+               if (next) {
+                  next.focus();
+               }
+            }
+         }
+      }).bind(this);
+
+      this._handleKeyUp = ((e) => {
+         const el = e.target;
+         if (!this.button.contains(el) && !this.menu.contains(el)) {
+            this.hide();
+         }
+      }).bind(this);
    }
 
    toggle() {
-      if (this.$el.hasClass(ENV_DROPDOWN_OPEN)) {
+      if (this.container.classList.contains(ENV_DROPDOWN_OPEN)) {
          this.hide();
       } else {
          this.show();
@@ -42,201 +107,68 @@ class Dropdown {
    }
 
    show() {
-      this.$el.addClass(ENV_DROPDOWN_OPEN);
-
-      this.$el.find(TOGGLE_DROPDOWN).attr('aria-expanded', 'true');
-
+      this.container.classList.add(ENV_DROPDOWN_OPEN);
+      this.button.setAttribute('aria-expanded', 'true');
+      document.body.appendChild(this.menu);
+      unhide(this.menu);
       this._bindEvents();
+      getPopper().then((Popper) => {
+         this._popper = new Popper(this.container, this.menu, {
+            placement: 'bottom-start',
+            modifiers: {
+               flip: {
+                  enabled: false,
+               },
+            },
+         });
+         this._popper.update();
+      });
    }
 
    hide() {
-      this.$el.removeClass(ENV_DROPDOWN_OPEN);
-
-      this.$el.find(TOGGLE_DROPDOWN).attr('aria-expanded', 'false');
-
+      this.container.classList.remove(ENV_DROPDOWN_OPEN);
+      this.button.setAttribute('aria-expanded', 'false');
       this._unbindEvents();
+      hide(this.menu);
+      this.placeholder.insertAdjacentElement('afterend', this.menu);
    }
 
    _bindEvents() {
-      const ENV_CLICK = `${ENV_CLICK_EVENT}-${this.el.id}`;
-      const ENV_KEYDOWN = `${ENV_KEYDOWN_EVENT}-${this.el.id}`;
-
-      $(document)
-         .off(ENV_CLICK)
-         .on(ENV_CLICK, (event) => {
-            if (!$(event.target).hasClass(ENV_DROPDOWN_MENU)) {
-               this.hide();
-            }
-         });
-
-      this.$el.off(ENV_KEYDOWN).one(ENV_KEYDOWN, (event) => {
-         if (event.key === KEYS.ESCAPE_KEY) {
-            this.hide();
-         }
-      });
+      this._unbindEvents();
+      document.addEventListener('click', this._handleClick);
+      document.addEventListener('keydown', this._handleKeyDown);
+      document.addEventListener('keyup', this._handleKeyUp);
    }
 
    _unbindEvents() {
-      $(document).off(`${ENV_CLICK_EVENT}-${this.el.id}`);
-      this.$el.off(`${ENV_KEYDOWN_EVENT}-${this.el.id}`);
+      document.removeEventListener('click', this._handleClick);
+      document.removeEventListener('keydown', this._handleKeyDown);
+      document.removeEventListener('keyup', this._handleKeyUp);
    }
+}
 
-   static _jQuery(config) {
-      return this.each(function () {
-         const $this = $(this);
-         let data = $this.data('env.dropdown');
-
-         if (!data) {
-            data = new Dropdown(this);
-            $this.data('env.dropdown', data);
-         }
-
-         if (typeof config === 'string') {
-            const method = data[config];
-            if (method === undefined) {
-               throw new Error(`No method named "${config}"`);
+if (document) {
+   document.addEventListener('click', async (e) => {
+      const button = e.target.closest(TOGGLE_DROPDOWN);
+      if (button) {
+         const container = e.target.closest(button.getAttribute(TARGET_ATTR));
+         const menu = container.querySelector(ENV_DROPDOWN_MENU);
+         if (container && container.id) {
+            if (container.getAttribute(DATA_INITIALIZED) === 'true') {
+               dropdowns[container.id].toggle();
+            } else if (container.getAttribute(DATA_INITIALIZED) !== 'true') {
+               await getPopper();
+               dropdowns[container.id] = new Dropdown(container, menu, button);
+               container.setAttribute(DATA_INITIALIZED, 'true');
+               if (menu) {
+                  menu.setAttribute(DATA_INITIALIZED, 'true');
+                  hide(menu);
+               }
+               dropdowns[container.id].toggle();
             }
-            method.call(data);
-            return;
-         }
-
-         data.toggle();
-      });
-   }
-}
-
-function selectMenuItem(event, $items) {
-   if (!$items.length) {
-      return;
-   }
-
-   let index = $items.index(event.target);
-
-   if (event.key === KEYS.ARROWUP && index > 0) {
-      index--;
-   }
-
-   if (event.key === KEYS.ARROWDOWN && index < $items.length - 1) {
-      index++;
-   }
-
-   index = index === -1 ? 0 : index;
-
-   $items[index].focus();
-}
-
-function clearMenus(event) {
-   if (event) {
-      if (
-         event.button === RIGHT_MOUSE_BUTTON ||
-         (event.type === 'keyup' && event.key !== KEYS.TAB_KEY)
-      ) {
-         return;
-      }
-   }
-
-   const $toggles = $(TOGGLE_DROPDOWN);
-
-   for (let i = 0, len = $toggles.length; i < len; i++) {
-      const data = $($toggles[i]).closest(ENV_DROPDOWN).data('env.dropdown');
-
-      if (!data) {
-         continue;
-      }
-      const dropdownMenu = data.$el.find(ENV_DROPDOWN_MENU)[0];
-
-      if (event) {
-         if (
-            event.originalEvent.path.includes(
-               document.documentElement.getElementsByClassName(
-                  'env-dropdown env-is-open'
-               )[0]
-            )
-         ) {
-            continue;
-         }
-
-         if (
-            event.type === 'keyup' &&
-            event.key === KEYS.TAB_KEY &&
-            dropdownMenu.contains(event.originalEvent.target)
-         ) {
-            continue;
          }
       }
-
-      data.hide();
-   }
-}
-
-function keydownHandler(event) {
-   if (!REGEXP_KEYDOWN.test(event.key)) {
-      return;
-   }
-
-   const isActive = $(this).closest(ENV_DROPDOWN).hasClass(ENV_DROPDOWN_OPEN);
-
-   if (!isActive && event.key === KEYS.ESCAPE_KEY) {
-      return;
-   }
-
-   event.preventDefault();
-   event.stopPropagation();
-
-   const getToggleButton = () =>
-      this.matches(TOGGLE_DROPDOWN)
-         ? this
-         : $(this).closest(ENV_DROPDOWN).find(TOGGLE_DROPDOWN)[0];
-
-   if (event.key === KEYS.ESCAPE_KEY) {
-      clearMenus();
-      getToggleButton().focus();
-
-      return;
-   }
-
-   if (
-      !isActive &&
-      (event.key === KEYS.ARROWUP || event.key === KEYS.ARROWDOWN)
-   ) {
-      getToggleButton().click();
-      return;
-   }
-
-   if (!isActive || event.which === KEYS.SPACE) {
-      clearMenus();
-
-      return;
-   }
-
-   const $dropdown = $(this).closest(ENV_DROPDOWN);
-
-   selectMenuItem(event, $dropdown.find('.env-dropdown__item'));
-}
-
-if (typeof document !== 'undefined') {
-   const NO_CONFLICT = $.fn[NAME];
-   $.fn[NAME] = Dropdown._jQuery;
-   $.fn[NAME].Constructor = Dropdown;
-   $.fn[NAME].noConflict = () => {
-      $.fn[NAME] = NO_CONFLICT;
-      return Dropdown._jQuery;
-   };
-
-   $(document).on('click', TOGGLE_DROPDOWN, function (e) {
-      e.preventDefault();
-
-      const $this = $(this);
-      const $target = $($this.data('target'));
-
-      $target[NAME]();
    });
-
-   $(document).on(ENV_KEYDOWN_EVENT, TOGGLE_DROPDOWN, keydownHandler);
-
-   $(document).on(ENV_KEYDOWN_EVENT, ENV_DROPDOWN_MENU, keydownHandler);
-
-   $(document).on(ENV_KEYUP_EVENT, clearMenus);
 }
 
 export default Dropdown;
