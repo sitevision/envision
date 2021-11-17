9d9e177dec906946b69835ffb70e141d72f8372e
Henrik Ekelöf <henrik.ekelof@sitevision.se>
2021-11-17 10:03:28 +0100

    Update copyIcons.js

--- packages/envision-docs/scripts/copyIcons.js
+++ packages/envision-docs/scripts/copyIcons.js
@@ -3,14 +3,6 @@ const fs = require('fs');
 const path = require('path');
 
 fs.copyFileSync(
-   path.resolve(
-      '..',
-      '..',
-      'packages',
-      'envision',
-      'src',
-      'icons',
-      'envision-icons.svg'
-   ),
+   path.resolve('..', '..', 'envision', 'dist', 'envision-icons.svg'),
    path.resolve(__dirname, '..', 'static', 'sitevision', 'envision-icons.svg')
 );
