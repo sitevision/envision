f18cce32b2c7bb6b0f3427cb00f6f01f663b3269
albinohrn <albinohrn@gmail.com>
2022-12-12 15:36:50 +0000

    Deploying to gh-pages from @ sitevision/envision@40c5773a6e07f5fe8d3a4ab6022efcb609aff1cf 🚀

--- component---src-pages-404-js-a0a6255f8860d38fd4fe.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkenvision_docs=self.webpackChunkenvision_docs||[]).push([[883],{4431:function(e,n,t){t.r(n);var a=t(2784);n.default=function(){return a.createElement("div",{className:"env-p-around--large"},a.createElement("h1",{className:"env-text"},"404 - Page not found"),a.createElement("p",{className:"env-text"},"Oops! That page does not exist. ",a.createElement("a",{href:"/"},"Home")))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-a0a6255f8860d38fd4fe.js.map
