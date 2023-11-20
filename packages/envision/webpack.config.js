f89154b79a619fff6045fcba1723b464b8f084f0
Henrik Ekelöf <henrik.ekelof@sitevision.se>
2023-11-20 17:33:00 +0100

    Prepare docs for dashboard

--- packages/envision/webpack.config.js
+++ packages/envision/webpack.config.js
@@ -12,102 +12,141 @@ const dev = process.env.NODE_ENV === 'development';
 
 console.log('Running dev mode', dev);
 
-module.exports = {
-   mode: dev ? 'development' : 'production',
-   devtool: 'source-map',
-   entry: './src/js/envision.js',
-   output: {
-      filename: 'envision.js',
-      path: dev
-         ? path.resolve(__dirname, '..', 'envision-docs', 'public', 'dist')
-         : path.resolve(__dirname, 'dist'),
-      library: {
-         name: 'envision',
-         type: 'umd',
-      },
-      globalObject: 'this',
-      publicPath: 'auto',
-      chunkFilename: '[name]-[contenthash].js',
-   },
-   optimization: {
-      minimizer: [new OptimizeCSSAssetsPlugin(), new TerserWebpackPlugin()],
-   },
-   plugins: [
-      new MiniCssExtractPlugin({
-         filename: 'envision.css',
-      }),
-      new ESLintPlugin(),
-      //compress using gzip
-      new CompressionPlugin({
-         test: /\.(js|css|json|html|svg)$/,
-      }),
-      //compress using brotli
-      new CompressionPlugin({
-         algorithm: 'brotliCompress',
-         test: /\.(js|css|json|html|svg)$/,
-         compressionOptions: {
-            params: {
-               [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
-            },
-         },
-      }),
-   ],
-   externals: {
-      jquery: {
-         commonjs: 'jquery',
-         commonjs2: 'jquery',
-         amd: 'jquery',
-         root: 'jQuery',
-      },
-   },
-   module: {
-      rules: [
-         {
-            test: /\.js$/,
-            include: path.resolve(__dirname, './src/js'),
-            use: [
-               {
-                  loader: 'babel-loader',
-                  options: {
-                     presets: [
-                        [
-                           '@babel/preset-env',
-                           {
-                              corejs: { version: '3' },
-                              useBuiltIns: 'usage',
-                           },
-                        ],
-                     ],
-                  },
-               },
-            ],
-         },
-         {
-            test: /\.scss$/,
-            use: [
-               MiniCssExtractPlugin.loader,
-               'css-loader',
-               {
-                  loader: 'css-vars-to-json',
-                  options: {
-                     filename: 'envision-css-vars.json',
-                  },
-               },
-               'postcss-loader',
-               'sass-loader',
-            ],
-         },
-         {
-            test: /\.(eot|svg|ttf|woff|woff2)$/,
-            use: [
-               {
-                  loader: 'file-loader',
-                  options: {
-                     name: '[name].[ext]',
-                  },
-               },
-            ],
-         },
-      ],
-   },
+const optimization = {
+   minimizer: [new OptimizeCSSAssetsPlugin(), new TerserWebpackPlugin()],
 };
+
+const commonPlugins = [
+   new ESLintPlugin(),
+   //compress using gzip
+   new CompressionPlugin({
+      test: /\.(js|css|json|html|svg)$/,
+   }),
+   //compress using brotli
+   new CompressionPlugin({
+      algorithm: 'brotliCompress',
+      test: /\.(js|css|json|html|svg)$/,
+      compressionOptions: {
+         params: {
+            [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+         },
+      },
+   }),
+];
+
+module.exports = [
+   {
+      mode: dev ? 'development' : 'production',
+      devtool: 'source-map',
+      entry: './src/scss/dashboard/envision-dashboard.scss',
+      output: {
+         filename: 'envision-dashboard.js',
+         path: dev
+            ? path.resolve(__dirname, '..', 'envision-docs', 'public', 'dist')
+            : path.resolve(__dirname, 'dist'),
+      },
+      optimization,
+      plugins: [
+         new MiniCssExtractPlugin({
+            filename: 'envision-dashboard.css',
+         }),
+         ...commonPlugins,
+      ],
+      module: {
+         rules: [
+            {
+               test: /\.scss$/,
+               use: [
+                  MiniCssExtractPlugin.loader,
+                  'css-loader',
+                  'postcss-loader',
+                  'sass-loader',
+               ],
+            },
+         ],
+      },
+   },
+   {
+      mode: dev ? 'development' : 'production',
+      devtool: 'source-map',
+      entry: './src/js/envision.js',
+      output: {
+         filename: 'envision.js',
+         path: dev
+            ? path.resolve(__dirname, '..', 'envision-docs', 'public', 'dist')
+            : path.resolve(__dirname, 'dist'),
+         library: {
+            name: 'envision',
+            type: 'umd',
+         },
+         globalObject: 'this',
+         publicPath: 'auto',
+         chunkFilename: '[name]-[contenthash].js',
+      },
+      optimization,
+      plugins: [
+         new MiniCssExtractPlugin({
+            filename: 'envision.css',
+         }),
+         ...commonPlugins,
+      ],
+      externals: {
+         jquery: {
+            commonjs: 'jquery',
+            commonjs2: 'jquery',
+            amd: 'jquery',
+            root: 'jQuery',
+         },
+      },
+      module: {
+         rules: [
+            {
+               test: /\.js$/,
+               include: path.resolve(__dirname, './src/js'),
+               use: [
+                  {
+                     loader: 'babel-loader',
+                     options: {
+                        presets: [
+                           [
+                              '@babel/preset-env',
+                              {
+                                 corejs: { version: '3' },
+                                 useBuiltIns: 'usage',
+                              },
+                           ],
+                        ],
+                     },
+                  },
+               ],
+            },
+            {
+               test: /\.scss$/,
+               use: [
+                  MiniCssExtractPlugin.loader,
+                  'css-loader',
+                  {
+                     loader: 'css-vars-to-json',
+                     options: {
+                        filename: 'envision-css-vars.json',
+                     },
+                  },
+                  'postcss-loader',
+                  'sass-loader',
+               ],
+            },
+            {
+               test: /\.(eot|svg|ttf|woff|woff2)$/,
+               use: [
+                  {
+                     loader: 'file-loader',
+                     options: {
+                        name: '[name].[ext]',
+                     },
+                  },
+               ],
+            },
+         ],
+      },
+   },
+];
