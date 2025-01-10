32c32067e62d1a7f80238a0c2b6227a7f608149a
Henrik Ekelöf <henrik.ekelof@sitevision.se>
2025-01-10 13:36:47 +0100

    Code review

--- packages/envision-docs/src/components/ThemePicker/index.js
+++ packages/envision-docs/src/components/ThemePicker/index.js
@@ -41,24 +41,6 @@ const ThemePicker = () => {
       changeScheme(e.target.checked ? 'dark' : 'light');
    };
 
-   // TODO: Decide if toggling dark mode via system setting should be implemented
-   // React.useEffect(() => {
-   //    const handleMqlColorSchemeChange = () => {
-   //       changeScheme(mqlColorScheme.matches ? 'dark' : 'light');
-   //    };
-   //
-   //    setTimeout(() => {
-   //       mqlColorScheme.addEventListener('change', handleMqlColorSchemeChange);
-   //    }, 100);
-   //
-   //    return () => {
-   //       mqlColorScheme.removeEventListener(
-   //          'change',
-   //          handleMqlColorSchemeChange
-   //       );
-   //    };
-   // }, [mqlColorScheme, changeScheme]);
-
    return (
       <div className="theme-picker">
          <label className="theme-picker__label" htmlFor="darkTheme">
