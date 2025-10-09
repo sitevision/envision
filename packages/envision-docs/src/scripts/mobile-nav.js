32cfca5b7a406ef962a7f7ce49519256ddbc264e
Henrik Ekelöf <henrik.ekelof@sitevision.se>
2025-10-09 11:28:41 +0200

    Update mobile-nav.js

--- packages/envision-docs/src/scripts/mobile-nav.js
+++ packages/envision-docs/src/scripts/mobile-nav.js
@@ -4,6 +4,50 @@ document.addEventListener('DOMContentLoaded', () => {
    const closeButton = document.querySelector('.mobileNav__close');
    let showMobileNav = false;
 
+   const isVisible = function (element) {
+      return !!(
+         element.offsetWidth ||
+         element.offsetHeight ||
+         element.getClientRects().length
+      );
+   };
+
+   const getFocusable = function (root) {
+      const SELECTOR = [
+         'a[href]',
+         'input:not([disabled])',
+         'button:not([disabled])',
+      ].join(',');
+      const allFocusable = root.querySelectorAll(SELECTOR);
+      const visibleFocusable = [];
+      for (let i = 0; i < allFocusable.length; i++) {
+         if (isVisible(allFocusable[i])) {
+            visibleFocusable.push(allFocusable[i]);
+         }
+      }
+      return visibleFocusable;
+   };
+
+   const handleKeyDown = (e) => {
+      if (e.key === 'Escape') {
+         e.preventDefault();
+         toggleMobileNav();
+      } else if (e.key === 'Tab') {
+         const focusable = getFocusable(mobileNav);
+         const firstElement = focusable[0];
+         const lastElement = focusable[focusable.length - 1];
+         if (e.shiftKey) {
+            if (e.target === firstElement) {
+               e.preventDefault();
+               lastElement.focus();
+            }
+         } else if (e.target === lastElement) {
+            e.preventDefault();
+            firstElement.focus();
+         }
+      }
+   };
+
    const toggleMobileNav = () => {
       if (!showMobileNav) {
          openButton.setAttribute('aria-expanded', 'true');
@@ -11,67 +55,20 @@ document.addEventListener('DOMContentLoaded', () => {
          document.body.style.setProperty('overflow-x', '');
          document.body.style.setProperty('overflow-y', '');
          document.body.style.setProperty('overflow', 'hidden', 'important');
+         document.body.addEventListener('keydown', handleKeyDown);
+         const focusable = getFocusable(mobileNav);
+         const firstLinkElement = focusable[1]; // Skip close button
+         firstLinkElement.focus();
       } else {
          openButton.setAttribute('aria-expanded', 'false');
          mobileNav.classList.remove('show');
          document.body.style.removeProperty('overflow');
+         document.body.removeEventListener('keydown', handleKeyDown);
+         openButton.focus();
       }
       showMobileNav = !showMobileNav;
    };
 
    closeButton && closeButton.addEventListener('click', toggleMobileNav);
    openButton && openButton.addEventListener('click', toggleMobileNav);
-
-   // TODO: HAndle keyboard navigation and focus trap
-
-   // const getFocusable = function (root) {
-   //    const SELECTOR = [
-   //       'a[href]',
-   //       'input:not([disabled])',
-   //       'button:not([disabled])',
-   //    ].join(',');
-   //    const allFocusable = root.querySelectorAll(SELECTOR);
-   //    const visibleFocusable = [];
-   //    for (let i = 0; i < allFocusable.length; i++) {
-   //       if (isVisible(allFocusable[i])) {
-   //          visibleFocusable.push(allFocusable[i]);
-   //       }
-   //    }
-   //    return visibleFocusable;
-   // };
-   //
-   // const handleKeyDown = (e) => {
-   //    if (e.key === 'Escape') {
-   //       setShowMobileNav(false);
-   //       mobileNav.classList.remove('show');
-   //       document.body.style.removeProperty('overflow');
-   //       e.preventDefault();
-   //    } else if (e.key === 'Tab') {
-   //       const focusable = getFocusable(mobileNav);
-   //       const firstElement = focusable[0];
-   //       const lastElement = focusable[focusable.length - 1];
-   //       if (e.shiftKey) {
-   //          if (e.target === firstElement) {
-   //             e.preventDefault();
-   //             lastElement.focus();
-   //          }
-   //       } else if (e.target === lastElement) {
-   //          e.preventDefault();
-   //          firstElement.focus();
-   //       }
-   //    }
-   // };
-   //
-   // if (showMobileNav) {
-   //    document.body.addEventListener('keydown', handleKeyDown);
-   // } else {
-   //    document.body.style.removeProperty('overflow');
-   // }
-   //
-   // return () => {
-   //    document.body.removeEventListener('keydown', handleKeyDown);
-   // };
-   /*
-    *
-    * */
 });
